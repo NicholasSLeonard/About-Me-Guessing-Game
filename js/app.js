97e0700cbf3056d31df3769555cafa8609fb2a9e
Nicholas Leonard <nicholas_leonard@live.com>
2022-06-12 19:38:37 -0400

    updated css styles, removed comments and console logs

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 'use strict';
 
 
@@ -6,8 +5,6 @@ let userName = prompt('What is your name?');
 
 alert('Hello ' + userName);
 
-// let btn = document.getElementById("myButton");
-// btn.addEventListener("click",quiz,false);
 
 let userScore = 0;
 question ();
@@ -18,62 +15,47 @@ function question() {
 
   alert('For this quiz type yes (or y) or no (or n)');
   let myLocation = validateYN(prompt('I grew up on the canary Islands, right?'), 'I grew up on the canary Islands, right?');
-  console.log('I grew up on the canary Islands, right?');
   if (myLocation === 'N' || myLocation === 'NO') {
     alert('Correct! I grew up on the Cayman Islands');
-    //console.log('correct!');
     userScore++;
   }
   else {
     alert('Incorrect! I grew up on the Cayman Islands');
-    //console.log('incorrect!');
   }
 
   let myEducation = validateYN(prompt('I was in the British education system, right?'), 'I was in the British education system, right?');
-  console.log('I was in the British education system, right?');
   if (myEducation === 'Y' || myEducation === 'YES') {
     alert('Correct! I was in the British education system');
-    //console.log('correct!');
     userScore++;
   }
   else {
     alert('Incorrect! I was in the British education system');
-    //console.log('incorrect!');
   }
   let myDegree = validateYN(prompt('I am working towards my Software Engineering degree, right?'), 'I am working towards my Software Engineering degree, right?');
-  console.log('I am working towards my Software Engineering degree, right?');
   if (myDegree === 'Y' || myDegree === 'YES') {
     alert('Incorrect! I am working towards my computer science degree');
-    //console.log('incorrect!');
   }
   else {
     alert('Correct! I am working towards my computer science degree');
-    //console.log('correct!');
     userScore++;
   }
 
   let myAlevels = validateYN(prompt('I took my A levels in college, right?'), 'I took my A levels in college, right?');
-  console.log('I took my A levels in college, right?');
   if (myAlevels === 'Y' || myAlevels === 'YES') {
     alert('Incorrect! I took my A levels in high school.');
-    //console.log('incorrect!');
   }
   else {
     alert('Correct! I took my A levels in high school.');
-    //console.log('correct!');
     userScore++;
   }
 
   let mySport = validateYN(prompt('I played basketball, right?'), 'I played basketball, right?');
-  console.log('I took my A levels in college, right?');
   if (mySport === 'N' || mySport === 'NO') {
     alert('Correct! I played tennis and baseball');
-    //console.log('correct!');
     userScore++;
   }
   else {
     alert('Incorrect! I played tennis and baseball');
-    //console.log('incorrect!');
   }
 
 }
@@ -83,21 +65,20 @@ function guessMyNumber() {
   let myNum = 5;
   let guess = 0;
   for (let i = 0; i < 4; i++) {
-    guess = prompt('I am thinking of a number between 1 and 10...');
-    console.log('I am thinking of a number between 1 and 10...');
+    guess = Number(prompt('I am thinking of a number between 1 and 10...'));
     if (guess > myNum) {
       alert('oops, too high');
     }
     else if (guess < myNum) {
       alert('oops, too low');
     }
-    else if (guess == myNum) {
+    else if (guess === myNum) {
       alert('WOW you got it!');
       userScore++;
       break;
     }
   }
-  if (guess != myNum) {
+  if (guess !== myNum) {
     alert('My number was ' + myNum);
   }
 }
@@ -107,10 +88,9 @@ function guessMyFavorites() {
   let friendGames = ['call of duty', 'league of legends', 'overwatch', 'halo', 'overwatch', 'csgo'];
   let userGuess;
   for (let i = 0; i < 6; i++) {
-    userGuess = prompt('Guess a video game I play with my friends.');
-    console.log('Guess a video game I play with my friends.');
+    userGuess = prompt('Guess a video game I play with my friends.').toLowerCase();
     for (let k = 0; k < 6; k++) {
-      if (userGuess == friendGames[k]) {
+      if (userGuess === friendGames[k]) {
         alert('Yay you got one. I play call of duty, league of legends, overwatch, halo, overwatch, and csgo with my friends');
         userScore++;
         i = 6;
